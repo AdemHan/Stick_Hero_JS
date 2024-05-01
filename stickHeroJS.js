0d4afb5546e4f97e5b08c422ebd82413c1d8a6f0
Adem Han <adem.han@outlook.com>
2024-05-01 16:46:02 +0300

    Yapılandırma ogeleri eklendi

--- stickHeroJS.js
+++ stickHeroJS.js
@@ -1,7 +1,7 @@
 // Javascript'in temel islevlerini genisletiyorum
 Array.prototype.last = function () {        // Dizinin son elemanini donduren bir fonksiyon
     return this[this.length - 1];
-  };
+};
   
   // Radyanlar yerine dereceleri kabul eden bir sinüs fonksiyonu yaziyorum
   Math.sinus = function (degree) {
@@ -22,3 +22,43 @@ Array.prototype.last = function () {        // Dizinin son elemanini donduren bi
 
   let score = 0;            // Skor girdisi
   
+  // Yapılandırma
+ const canvasWidth = 375;               // Tuval genisligi
+ const canvasHeight = 375;              // Tuval yuksekligi
+ const platformHeight = 100;            // Platform yuksekligi
+ const heroDistanceFromEdge = 10;       // Kenardan kahramanin uzakligi (beklerken)
+ const paddingX = 100;                  // Kahramanin orijinal tuval boyutundan beklemesi gereken konumu
+ const perfectAreaSize = 10;            // Mükemmel bolge boyutu
+
+// Arka plan, kahramandan daha yavas hareket eder
+const backgroundSpeedMultiplier = 0.2;  // Arka planın hızını ayarlayan çarpan
+
+// Tepenin 1. kısmı için parametreler
+const hill1BaseHeight = 100;            // Tepenin taban yüksekligi
+const hill1Amplitude = 10;              // Tepenin genisligi
+const hill1Stretch = 1;                 // Tepenin uzatılma oranı
+
+// Tepenin 2. kısmı için parametreler
+const hill2BaseHeight = 70;             // Tepenin taban yüksekligi
+const hill2Amplitude = 20;              // Tepenin genisligi
+const hill2Stretch = 0.5;               // Tepenin uzatilma orani
+
+const stretchingSpeed = 4;              // Bir piksel cizmek icin gecen milisaniye sayisi
+const turningSpeed = 4;                 // Bir derece döndürmek icin gecen milisaniye sayisi
+const walkingSpeed = 4;                 // Yürüme hızı
+const transitioningSpeed = 2;           // Gecis hızı
+const fallingSpeed = 2;                 // Dusme hızı
+
+const heroWidth = 17;                   // Kahramanın genisligi
+const heroHeight = 30;                  // Kahramanın yüksekligi
+
+const canvas = document.getElementById("game");
+canvas.width = window.innerWidth;       // Tuvali tam ekran yapiyorum
+canvas.height = window.innerHeight;
+
+const ctx = canvas.getContext("2d");    // Tuval icin 2D baglam olusturuyorum
+
+const introductionElement = document.getElementById("introduction");    // Tanitim elementi
+const perfectElement = document.getElementById("perfect");              // Mukemmel element
+const restartButton = document.getElementById("restart");               // Yeniden baslatma dügmesi
+const scoreElement = document.getElementById("score");                  // Skor elementi
